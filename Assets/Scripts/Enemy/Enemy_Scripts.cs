0cd8110206f0951721c16ffd3e805473861afa71
orewaminhdesu <minhhnim134@gmail.com>
2025-12-14 01:37:27 +0700

    add decor Rung Than Khoc

--- Assets/Scripts/Enemy/Enemy_Scripts.cs
+++ Assets/Scripts/Enemy/Enemy_Scripts.cs
@@ -1,3 +1,160 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [Header("Movement")]
+    [SerializeField] private int direction = 1; // -1: left, 1: right
+    [SerializeField] private float patrolSpeed = 3f;
+    [SerializeField] private Transform checkPoint;
+    [SerializeField] private float groundCheckDistance = 5f;
+    [SerializeField] private LayerMask groundMask;
+    private bool facingRight = true;
+
+    [Header("Player Chase")]
+    [SerializeField] private Transform player;
+    [SerializeField] private float chaseRange = 6f;
+    [SerializeField] private float chaseStopDistance = 2.5f;
+    [SerializeField] private float chaseSpeed = 5f;
+
+    [Header("Combat")]
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRadius = 1f;
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private float maxHealth = 5f;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+
+    private bool inRange;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (maxHealth <= 0)
+        {
+            Die();
+            return;
+        }
+
+        inRange = Vector2.Distance(transform.position, player.position) <= chaseRange;
+
+        if (inRange)
+            HandleChase();
+        else
+            HandlePatrol();
+    }
+
+    //
+    // PATROL
+    //
+    private void HandlePatrol()
+    {
+        animator.SetBool("Run", true);
+  
+        rb.linearVelocity =new Vector2( direction * patrolSpeed, rb.linearVelocity.y);
+        // Check ground
+        bool groundDetected = Physics2D.Raycast(checkPoint.position, Vector2.down, groundCheckDistance, groundMask);
+
+        if (!groundDetected)
+            Flip();
+    }
+
+    //
+    // Chase and Attack
+    //
+    private void HandleChase()
+    {
+        FacePlayer();
+
+        float dist = Vector2.Distance(transform.position, player.position);
+
+        if (dist > chaseStopDistance)
+        {
+            animator.SetBool("Run", true);
+            transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+            Attack();
+        }
+    }
+
+    private void FacePlayer()
+    {
+        if (player.position.x > transform.position.x && !facingRight)
+            Flip();
+        else if (player.position.x < transform.position.x && facingRight)
+            Flip();
+    }
+
+    // 
+    // Attack
+    //
+    private void Attack()
+    {
+        animator.SetTrigger("Attack");
+
+        Collider2D col = Physics2D.OverlapCircle(attackPoint.position, attackRadius, playerMask);
+        if (col != null && col.TryGetComponent<PlayerController>(out var playerController))
+        {
+            playerController.takeDamage(1);
+        }
+    }
+
+    // 
+    // Hurt & Die
+    // 
+    public void TakeDamage(int damage)
+    {
+        if (maxHealth <= 0) return;
+
+        maxHealth -= damage;
+        if (maxHealth <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("Died");
+        Destroy(gameObject, 0.3f); // Chờ animation 1 chút
+    }
+
+    //
+    // UTIL
+    // 
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.eulerAngles = facingRight ? Vector3.zero : new Vector3(0, 180, 0);
+        direction *= -1;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Ground check
+        if (checkPoint != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(checkPoint.position, Vector2.down * groundCheckDistance);
+        }
+
+        // Chase range
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+
+        // Attack
+        if (attackPoint != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+        }
+    }
+}
 // using Unity.VisualScripting;
 // using UnityEngine;
 // using UnityEngine.UI;
@@ -154,161 +311,3 @@
 //     }
 
 // }
-
-using UnityEngine;
-
-public class Enemy : MonoBehaviour
-{
-    private Rigidbody2D rb;
-    [Header("Movement")]
-    [SerializeField] private int direction = 1; // -1: left, 1: right
-    [SerializeField] private float patrolSpeed = 3f;
-    [SerializeField] private Transform checkPoint;
-    [SerializeField] private float groundCheckDistance = 5f;
-    [SerializeField] private LayerMask groundMask;
-    private bool facingRight = true;
-
-    [Header("Player Chase")]
-    [SerializeField] private Transform player;
-    [SerializeField] private float chaseRange = 6f;
-    [SerializeField] private float chaseStopDistance = 2.5f;
-    [SerializeField] private float chaseSpeed = 5f;
-
-    [Header("Combat")]
-    [SerializeField] private Transform attackPoint;
-    [SerializeField] private float attackRadius = 1f;
-    [SerializeField] private LayerMask playerMask;
-    [SerializeField] private float maxHealth = 5f;
-
-    [Header("Animation")]
-    [SerializeField] private Animator animator;
-
-    private bool inRange;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-    void Update()
-    {
-        if (maxHealth <= 0)
-        {
-            Die();
-            return;
-        }
-
-        inRange = Vector2.Distance(transform.position, player.position) <= chaseRange;
-
-        if (inRange)
-            HandleChase();
-        else
-            HandlePatrol();
-    }
-
-    //
-    // PATROL
-    //
-    private void HandlePatrol()
-    {
-        animator.SetBool("Run", true);
-  
-        rb.linearVelocityX = direction* Time.deltaTime * patrolSpeed;
-        // Check ground
-        bool groundDetected = Physics2D.Raycast(checkPoint.position, Vector2.down, groundCheckDistance, groundMask);
-
-        if (!groundDetected)
-            Flip();
-    }
-
-    //
-    // Chase and Attack
-    //
-    private void HandleChase()
-    {
-        FacePlayer();
-
-        float dist = Vector2.Distance(transform.position, player.position);
-
-        if (dist > chaseStopDistance)
-        {
-            animator.SetBool("Run", true);
-            transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
-        }
-        else
-        {
-            animator.SetBool("Run", false);
-            Attack();
-        }
-    }
-
-    private void FacePlayer()
-    {
-        if (player.position.x > transform.position.x && !facingRight)
-            Flip();
-        else if (player.position.x < transform.position.x && facingRight)
-            Flip();
-    }
-
-    // 
-    // Attack
-    //
-    private void Attack()
-    {
-        animator.SetTrigger("Attack");
-
-        Collider2D col = Physics2D.OverlapCircle(attackPoint.position, attackRadius, playerMask);
-        if (col != null && col.TryGetComponent<PlayerController>(out var playerController))
-        {
-            playerController.takeDamage(1);
-        }
-    }
-
-    // 
-    // Hurt & Die
-    // 
-    public void TakeDamage(int damage)
-    {
-        if (maxHealth <= 0) return;
-
-        maxHealth -= damage;
-        if (maxHealth <= 0)
-            Die();
-    }
-
-    private void Die()
-    {
-        animator.SetTrigger("Died");
-        Destroy(gameObject, 0.3f); // Chờ animation 1 chút
-    }
-
-    //
-    // UTIL
-    // 
-    private void Flip()
-    {
-        facingRight = !facingRight;
-        transform.eulerAngles = facingRight ? Vector3.zero : new Vector3(0, 180, 0);
-        direction *= -1;
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        // Ground check
-        if (checkPoint != null)
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawRay(checkPoint.position, Vector2.down * groundCheckDistance);
-        }
-
-        // Chase range
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, chaseRange);
-
-        // Attack
-        if (attackPoint != null)
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
-        }
-    }
-}
